aa7c4cc4712ddb7f4cf8deb40c3f9717041c4367
Sohaib Rashid <sohaib@gmail.com>
2018-09-13 10:13:28 +0500

    history page seprate views

---
+++ application/views/history/history_items.php
@@ -0,0 +1,97 @@
+<div class="tab-pane" id="items" role="tabpanel">
+	<div class="card">
+		<div class="card-header">
+			<div class="row">
+				<div class="col-lg-2 col-md-3" >
+					<div class="form-group">
+						<label>Item Name:</label>
+    					<input type="text" class="form-control" name="" >
+					</div>
+				</div>
+				<div class=" col-lg-3 col-md-4" >
+					<div class="form-group">
+						<label>Category:</label>
+    					<select class="form-control">
+    						<option>Select</option>
+    						<option>GIT Problem</option>
+    						<option>Co-Resp</option>
+						</select>
+					</div>
+				</div>
+				<div class="col-lg-2 col-md-4 p-0">
+    				<div class="form-group m-t-25" style="display: inline-flex;">
+    					<button class="btn btn-sm btn-primary">Add</button>
+    					<div class="custom-file">
+						    <input type="file" class="custom-file-input hide" id="inputGroupFile04" >
+						    <label class="btn btn-info btn-sm" for="inputGroupFile04">Add Multiple</label>
+						 </div>
+					</div>
+				</div>
+				<div class="col-lg-3 col-md-4">
+					<div class="form-group ">
+						<label>Select Category:</label>
+    					<select class="form-control">
+    						<option>Select</option>
+    						<option>GIT Problem</option>
+    						<option>Co-Resp</option>
+						</select>
+					</div>
+				</div>
+				<div class="col-lg-2 col-md-3">
+					<div class="custom-file m-t-25">
+					    <input type="file" class="custom-file-input hide" id="inputGroupFile04" >
+					    <label class="btn btn-info btn-sm" for="inputGroupFile04">Export items</label>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="card-body">
+			<table class="table table-bordered nowrap responsive" cellspacing="0" id="" width="100%" >
+               <thead>
+                <tr>
+                    <th style="width: 10%">Delete</th>
+                    <th style="width: 10%">Discription</th>
+                    <th>Item Name</th>
+                </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td style="width: 10%"><i class="fa fa-trash"></i></td>
+                        <td style="width: 10%" data-toggle="modal" data-target="#history-modal"><i class="far fa-question-circle"></i></td>
+                        <td>Rist Factor</td>
+                    </tr>
+                    <tr>
+                        <td style="width: 10%"><i class="fa fa-trash"></i></td>
+                        <td style="width: 10%" data-toggle="modal" data-target="#history-modal"><i class="far fa-question-circle"></i></td>
+                        <td>GIT Problem</td>
+                    </tr>
+                    <tr>
+                        <td style="width: 10%"><i class="fa fa-trash"></i></td>
+                        <td style="width: 10%" data-toggle="modal" data-target="#history-modal"><i class="far fa-question-circle"></i></td>
+                        <td>Co-Resp</td>
+                    </tr>
+                    <tr>
+                        <td style="width: 10%"><i class="fa fa-trash"></i></td>
+                        <td style="width: 10%" data-toggle="modal" data-target="#history-modal"><i class="far fa-question-circle"></i></td>
+                        <td>Co-GIT</td>
+                    </tr>
+                    <tr>
+                        <td style="width: 10%"><i class="fa fa-trash"></i></td>
+                        <td style="width: 10%" data-toggle="modal" data-target="#history-modal"><i class="far fa-question-circle"></i></td>
+                        <td>Co-CNS</td>
+                    </tr>
+                    <tr>
+                        <td style="width: 10%"><i class="fa fa-trash"></i></td>
+                        <td style="width: 10%" data-toggle="modal" data-target="#history-modal"><i class="far fa-question-circle"></i></td>
+                        <td>Co-CVS</td>
+                    </tr>
+                    <tr>
+                        <td style="width: 10%"><i class="fa fa-trash"></i></td>
+                        <td style="width: 10%" data-toggle="modal" data-target="#history-modal"><i class="far fa-question-circle"></i></td>
+                        <td>Respitatory Problems</td>
+                    </tr>
+                </tbody>
+            </table>
+		</div>
+	</div>
+</div>
